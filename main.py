71d475f1f64e7cfd434419275301ddff67bbca1a
Koromonnnnnnnn <764363@dpsk12.net>
2024-04-10 09:23:21 -0600

    .

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import player
 
 pygame.init()
 pygame.display.set_caption("top down grid game")
@@ -6,47 +7,8 @@ screen = pygame.display.set_mode((1000, 800))
 clock = pygame.time.Clock()
 gameover = False
 
-
+p1 = player()
 FPS = 60
-
-LEFT = 0
-RIGHT = 1
-UP = 2
-DOWN = 3
-SPACE = 4
-W = 5
-
-
-class player:
-    def __init__(self):
-
-        self.xpos = 400
-        self.ypos = 415
-        self.vx = 0
-        self.vy - 0
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, (255, 0, 255), (self.xpos, self.ypos, 30, 30))
-
-
-def move(self, keys, map):
-
-    if keys[LEFT] == True:
-        self.vx = -3
-        print("Moving left")
-    elif keys[RIGHT] == True:
-        self.vx = 3
-        print("Moving right")
-    else:
-        self.vx = 0
-
-    if map[int((self.ypos - 10) / 50)][int((self.xpos - 10) / 50)] == 2:
-        self.xpos += 3
-    if map[int((self.ypos) / 50)][int((self.xpos + 30 + 5) / 50)] == 2:
-        self.xpos -= 3
-
-    self.xpos += self.vx
-
 
 map = [
     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
@@ -73,10 +35,12 @@ while not gameover:
             gameover = True
 
     # physics section
+    p1.move(keys, map)
 
     # render section
 
     screen.fill((0, 0, 0))
+    p1.draw(screen)
 
     # draw map
     for i in range(10):
