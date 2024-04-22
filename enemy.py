2b42cfebf9bb34ca30f461cb6c48939496fa72aa
Koromonnnnnnnn <764363@dpsk12.net>
2024-04-22 09:17:06 -0600

    v

--- enemy.py
+++ enemy.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 LEFT = 0
 RIGHT = 1
@@ -7,4 +8,33 @@ DOWN = 3
 SPACE = 4
 W = 5
 
-#do yesterday gimkit
+enemy = pygame.image.load("enemy.png")
+ticker = 0
+
+class enemy:
+    def __init__(self):
+        self.xpos = 400
+        self.ypos = 200
+        self.direction = RIGHT
+
+    def draw(self, screen):
+        screen.blit(enemy, (self.xpos, self.ypos, 20, 20))
+    
+    def move(self):
+        if ticker % 40 == 0:
+            num = random.randrange(0,4)
+            if num == 0:
+                self.direction = RIGHT
+            elif num == 1:
+                self.direction = LEFT
+            elif num == 2:
+                self.direction = UP
+            elif num == 3:
+                self.direction = DOWN
+        if abs(int(py/50) - int(self.ypos/50)) < 2:
+            if px < self.xpos:
+                self.xpos -= 5
+                self.direction = LEFT
+            else:
+                self.xpos += 5
+                self.direction = RIGHT
